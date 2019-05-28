40cb7a2750746866521832496d974f1c7863c3c1
GPointExtreme <dominik.geiger@hotmail.com>
2019-05-28 12:04:28 +0200

    eLearning3 Log eingebaut

--- Uebung-01a-2-Weitere-Regressionsrechnungen.R
+++ Uebung-01a-2-Weitere-Regressionsrechnungen.R
@@ -63,17 +63,33 @@ hist(body$Work.hours)
 plot(body$Function.point, body$Work.hours)
 #Viele Datensätze sind in der linken unteren Ecke
 
-#4
-model = lm(Work.hours ~ Function.point, data = body)
+#4 ohne Log:
+model = lm(Work.hours.log ~ Function.point.log, data = body)
 koeff = coef(model)
 abline(a=koeff[1], b=koeff[2], col = "blue")
+
+#4 mit Log:
+#Log anwenden um Punkte aus linken unterer Ecke zu bekommen
+model.log=lm(log(Work.hours) ~ log(Function.point), data = body)
+hist(resid(model.log))
+plot(log(body$Function.point),log(body$Work.hours))
+#plot sieht nun schon viel Besser aus
+
+koeff.log = coef(model.log)
+exp(koeff.log)
+exp(confint(model.log))
+abline(model.log)
+#Bei erhöhung der points um 1 Einheit erhöht sich die hours um 2,724778
+#Der Konfidenzintervall von 95% beim intercept liegt zwischen 4,021764 und 37,984103
+#nnd bei den Functionspoints zwischen 2,266963  und 3,275049 h/functionpoints
 
 #4a
 summary(model)
 confint(model)
-#Bei erhöhung der points um 1 Einheit erhöhen sich die Stunden um 15,124
+#ohne Log: Bei erhöhung der points um 1 Einheit erhöhen sich die Stunden um 15,124
 
 #4b
+#ohne Log:
 predict(model, newdata = data.frame(Function.point = 100, type='response'))
 #2098.09
 predict(model, newdata = data.frame(Function.point = 200, type='response'))
@@ -86,3 +102,18 @@ predict(model, newdata = data.frame(Function.point = 1600, type='response'))
 #24784.48
 predict(model, newdata = data.frame(Function.point = 3200, type='response'))
 #48983.3
+
+#Mit Log:
+#Aufpassen hier! Variable in Dataframe darf nicht die mit .log sein!
+exp(predict(model.log, newdata = data.frame(Function.point=100),type="response"))
+#1249.634
+exp(predict(model.log, newdata = data.frame(Function.point=200),type="response"))
+#2503.406
+exp(predict(model.log, newdata = data.frame(Function.point=400),type="response"))
+#5015.103
+exp(predict(model.log, newdata = data.frame(Function.point=800),type="response"))
+#10046.81
+exp(predict(model.log, newdata = data.frame(Function.point=1600),type="response"))
+#20126.9
+exp(predict(model.log, newdata = data.frame(Function.point=3200),type="response"))
+#40320.45
